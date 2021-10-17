6629334a4dc1a3ebd8e7d79fe07e35b9c3de9a82
maxarvid <max.arvid.anderson@gmail.com>
2021-10-17 19:22:56 +0200

    Sets custom business hours

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 
 const App = () => {
+
+  const businessHours = {
+    daysOfWeek: [1, 2, 3, 5, 4, 6, 7],
+    startTime: '07:00',
+    endTime: '19:00'
+  }
   return (
     <div>
       <h1>Hello laundry world!</h1>
@@ -11,10 +17,11 @@ const App = () => {
         plugins={[dayGridPlugin, timeGridPlugin]}
         initialView="timeGridWeek"
         headerToolbar={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'timeGridWeek,timeGridDay'
+          left: "prev,next today",
+          center: "title",
+          right: "timeGridWeek,timeGridDay",
         }}
+        businessHours={businessHours}
       />
     </div>
   );
