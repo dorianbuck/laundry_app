2465aaeac1556832195626e84d4b7a66bdb6382a
maxarvid <max.arvid.anderson@gmail.com>
2021-10-17 18:50:53 +0200

    Fixes cypress baseUrl, adds fullcalendar react dependency

---
+++ cypress/integration/userCanViewWeeklySchedule.feature.js
@@ -0,0 +1,9 @@
+describe('Visitor views the current weeks schedule', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('is expected to display 7 days calendar', () => {
+    
+  });
+})
